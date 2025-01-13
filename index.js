4b8a9926f62e368c4e6de4abac3c84777b4c69ef
Nikao1 <nikolasadsms@gmail.com>
2025-01-13 11:38:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+class Heroi{
+    constructor(nome, idade, tipo){
+    
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+}
+
+atacar(){
+    let ataque = "";
+
+    if (this.tipo === "mago"){
+        ataque = "magia";
+    }   else if(this.tipo === "guerreiro"){
+        ataque = "espada";
+    }   else if(this.tipo === "monge"){
+        ataque = "artes marciais";
+    }   else if(this.tipo === "ninja"){
+        ataque = "shuriken";
+    }   else{
+        ataque = "ataque desconhecido"
+    }        
+
+    console.log(`O/A ${this.tipo} atacou usando o ${ataque}`);
+}
+}
+
+const heroi1 = new Heroi("Frieren", "?", "maga");
+const heroi2 = new Heroi("Stark", 16, "guerreiro");
+const heroi3 = new Heroi("Lee Sin", 40, "monge");
+const heroi4 = new Heroi("Zed", 25, "ninja");
+
+heroi1.atacar(); 
+heroi2.atacar();  
+heroi3.atacar();  
+heroi4.atacar();  
